d137601097f5e913495cc07ddeaa5f2ceeda1c4f
arthurd3 <arthur_camposl@yahoo.com>
2025-09-12 12:33:28 -0300

    feat: create docker compose to mongoDB for AI service

---
+++ aiservice/mongo-init/init-db.js
@@ -0,0 +1,6 @@
+
+db = db.getSiblingDB('fitnessrecomendation');
+
+db.createCollection('recomendations');
+
+print("Database 'fitnessrecomendation' and collections 'recomendations' is created with success.");
