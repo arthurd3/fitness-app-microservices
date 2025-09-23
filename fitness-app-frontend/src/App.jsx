2263e3ffdde8d8f42bc2e9439a6d2f9e406611c8
arthurd3 <arthur_camposl@yahoo.com>
2025-09-23 15:53:08 -0300

    feat: authSlice and JWT State configure

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import { Button } from "@mui/material"
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { BrowserRouter as Router , Navigate , Route , Routes , useLocation} from "react-router"
+import { setCredentials } from "./store/authSlice";
 
 function App() {
+
+  const {token, tokenData , logIn , logOut , isAuthenticated } = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady , setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if(token){
+      dispatch(setCredentials({token , user: tokenData}))
+      setAuthReady(true);
+    }
+  }, [token , tokenData , dispatch])
+
   return (
     <Router>
       <Button variant="contained" color="#dc004e">Login </Button>
